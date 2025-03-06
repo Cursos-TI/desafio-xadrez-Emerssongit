15d676f0466216caf9a2c76d4527de0d0955ecac
Emerssongit <emerssonbarbosaramos@gmail.com>
2025-03-06 22:41:54 +0000

    xadrez

--- Desafio_XADREZ/Xadrez_N1.c
+++ Desafio_XADREZ/Xadrez_N1.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 
 int main(){
-    
+
+    //variaveis bispo e rainha.
     int c = 1;
     int b = 1;
 
-
+     // TORRE, SIMULA MOVIMENTO horizontalmente ou verticalmente, Sendo aqui horizontal a direita.
+    printf("MOVIMENTO DA TORRE.\n");
     for (int i = 1; i <= 5; i++)
     {
-    printf("direita\n");
+        printf("Direita\n");
     }
-
+     // BISPO, simula movimento em diagonal, em cima direita 5 casas.
+    printf("MOVIMENTO DO BISPO.\n");
     while (c <= 5) {
-        printf("CIMA\n", &c);
+
+        printf("Cima, Direita.\n", &c);
         c++;
     }
-
+     // RAINHA, move em todas direções, 8 casas a esquerda horizontal.
+    printf("MOVIMENTO DA RAINHA.\n");
     do {
-
-        printf("ESQUERDA\n", &b);
+        
+        printf("Esquerda\n", &b);
         b++;
 
-    
-
-    } while ( b <= 5 );
+    } while ( b <= 8 );
 
     return 0;
 }
